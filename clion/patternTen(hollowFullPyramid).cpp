d07c533653fec9bfb0b3ed2ee2e823523b099bd1
chaudhuree <chaudhuree00@gmail.com>
2024-07-18 03:31:16 +0600

    fix some name typo and code format

--- clion/patternTen(hollowFullPyramid).cpp
+++ clion/patternTen(hollowFullPyramid).cpp
@@ -19,6 +19,7 @@
 //*******
 
 #include <iostream>
+
 using namespace std;
 
 int main() {
@@ -27,23 +28,23 @@ int main() {
     cin >> N;
     cout << endl;
 
-    for(int row =0; row<N; row++){
-        int k=0;
-        for(int col=0; col<(2*N)-1; col++){
-            if(col<N-row-1){
-                cout<<" ";
-            }else if(k< 2*row +1){
-                if(k==0 || k==2*row || row==N-1){
-                    cout<<"*";
-                }else{
-                    cout<<" ";
+    for (int row = 0; row < N; row++) {
+        int k = 0;
+        for (int col = 0; col < (2 * N) - 1; col++) {
+            if (col < N - row - 1) {
+                cout << " ";
+            } else if (k < 2 * row + 1) {
+                if (k == 0 || k == 2 * row || row == N - 1) {
+                    cout << "*";
+                } else {
+                    cout << " ";
                 }
                 k++;
-            }else{
-                cout<<" ";
+            } else {
+                cout << " ";
             }
         }
-        cout<<endl;
+        cout << endl;
     }
 
     return 0;
