286a70bb8c8fd5e8b8d34d8767650ffeafbafbc9
chaudhuree <chaudhuree00@gmail.com>
2024-07-30 17:53:40 +0600

    hollow numeric inverted half pyramid 1 2 3 4 5 1     4 1   3 1 2 1

--- clion/patterns/patternThirteen(hollow numeric inverted half pyramid).cpp
+++ clion/patterns/patternThirteen(hollow numeric inverted half pyramid).cpp
@@ -1,3 +1,12 @@
+//
+// Created by chaudhuree on 7/30/2024.
+//
+//1 2 3 4 5
+//1     4
+//1   3
+//1 2
+//1
+//
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -7,7 +16,7 @@ int main() {
 
     for (int row = 0; row < n; row++) {
         for (int col = 0; col < n-row; col++) {
-            if (col == 0 || row==0 || col==n-row-1) {
+            if (col == 0 || row==0 || col==n-row-1) {  //first row,first col, last col== n-row-1
                 cout << col + 1 << " ";
             } else {
                 cout << " " << " ";
